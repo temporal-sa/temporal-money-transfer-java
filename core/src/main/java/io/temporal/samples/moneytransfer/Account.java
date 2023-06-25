7d61730b545300d83de4d052d204ccd9509c67b6
Steve Androulakis <steve.androulakis@gmail.com>
2023-06-25 10:29:43 -0700

    added missing account.java

---
+++ core/src/main/java/io/temporal/samples/moneytransfer/Account.java
@@ -0,0 +1,49 @@
+/*
+ *  Copyright (c) 2020 Temporal Technologies, Inc. All Rights Reserved
+ *
+ *  Copyright 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Modifications copyright (C) 2017 Uber Technologies, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ *  use this file except in compliance with the License. A copy of the License is
+ *  located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ *  or in the "license" file accompanying this file. This file is distributed on
+ *  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *  express or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+package io.temporal.samples.moneytransfer;
+
+public class Account {
+
+  private String accountId;
+  private int balance;
+
+  public Account() {}
+
+  public Account(String accountId, int balance) {
+    this.accountId = accountId;
+    this.balance = balance;
+  }
+
+  public String getAccountId() {
+    return accountId;
+  }
+
+  public int getBalance() {
+    return balance;
+  }
+
+  public void setBalance(int balance) {
+    this.balance = balance;
+  }
+
+  @Override
+  public String toString() {
+    return accountId + " ($" + balance + ")";
+  }
+}
